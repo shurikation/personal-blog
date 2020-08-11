c1f307870b3efb902f0643d2e64251db6dc22a26
shurikation <shurikation@gmail.com>
2020-08-11 21:15:00 +0400

    Wrote and refactored auto-text component - automatically typing any text on the page

--- src/auto-text.js
+++ src/auto-text.js
@@ -1,58 +1,43 @@
-
 export class AutoText {
   constructor(props) {
-    this.text = props.text;
+    this.arrOfLetters = props.text.split("");
     this.$elem = document.querySelector(props.elem);
 
-    this.arrayOfSymbols = this.text.split("");
+    this.slowTypingSpeed = 400; //ms
+    this.fastTypingSpeed = 100; //ms
 
-    this.firstPartOfArray = Math.floor(this.arrayOfSymbols.length / 5);
-    this.secondPartOfArray = Math.floor(this.arrayOfSymbols.length) - this.firstPartOfArray;
-    this.slowTimeOut = 400;
-    this.fastTimeOut = 100;
+    this.slowTypingDistance = 3; //symbols
+    this.fastTypingDistance = 6; //symbols
 
-    this.slowPart();
+    this.init();
   }
 
-  slowPart(count) {
-
-
-   let counter = count || 0;
-   console.log(count);
-
-   const ID = setInterval(() => {
-     this.showSymbol(this.arrayOfSymbols[counter]);
-     counter++;
-     if(counter === this.firstPartOfArray) {
-       clearInterval(ID);
-       this.fastPart(counter);
-     } else if (counter >= this.arrayOfSymbols.length) {
-       clearInterval(ID);
-     }
-   }, this.slowTimeOut);
-
-
-    // arrayOfSymbols.forEach(symbol => {
-    //   setInterval(() => console.log(symbol), 2000);
-    // });
-
-
+  init() {
+    this.textTypingHandler();
   }
 
-  showSymbol(symbol) {
+  textTypingHandler(speed = this.slowTypingSpeed, distance = this.slowTypingDistance, value = 0) {
+    let mainCounter = value;
+    let distanceCounter = 0;
+
+    const ID = setInterval(() => {
+      this.displaySymbol(this.arrOfLetters[mainCounter]);
+      mainCounter++;
+      distanceCounter++;
+
+      if (mainCounter >= this.arrOfLetters.length) clearInterval(ID);
+
+      if (distanceCounter >= distance && distance === this.slowTypingDistance) {
+        clearInterval(ID);
+        this.textTypingHandler(this.fastTypingSpeed, this.fastTypingDistance, mainCounter);
+      } else if (distanceCounter >= distance && distance === this.fastTypingDistance) {
+        clearInterval(ID);
+        this.textTypingHandler(this.slowTypingSpeed, this.slowTypingDistance, mainCounter);
+      }
+    }, speed);
+  }
+
+  displaySymbol(symbol) {
     this.$elem.insertAdjacentText('beforeEnd', symbol);
   }
-
-  fastPart(counter) {
-    let newCounter = this.firstPartOfArray;
-    const ID = setInterval(() => {
-      this.showSymbol(this.arrayOfSymbols[newCounter]);
-      newCounter++;
-      if(newCounter >= this.secondPartOfArray) {
-        clearInterval(ID);
-        this.slowPart(newCounter);
-      }
-    }, this.fastTimeOut);
-  }
-
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-import {ScrollHandler} from "./scroll-handler";
-import {Header} from "./components/header/header.component";
+// import {ScrollHandler} from "./scroll-handler";
+// import {Header} from "./components/header/header.component";
+import {AutoText} from "./auto-text";
 
-const scrollHandler = new ScrollHandler();
+// const scrollHandler = new ScrollHandler();
 
-export const header = new Header({
-  header: '.main-header',
-  nav: '.main-header__nav',
-  logo: '.main-header__logo',
-  menu: '.menu',
-  links: '.menu__link',
-  button: '.header__toggler',
-  active: 'toggler--active',
-  open: 'menu--opened',
-  fixed: 'header--fixed',
-  unfixed: 'header--unfixed',
-  black: "#1e1e1e",
-  white: "#fff",
-  animationTriggerElem: '.intro__title'
+// export const header = new Header({
+//   header: '.main-header',
+//   nav: '.main-header__nav',
+//   logo: '.main-header__logo',
+//   menu: '.menu',
+//   links: '.menu__link',
+//   button: '.header__toggler',
+//   active: 'toggler--active',
+//   open: 'menu--opened',
+//   fixed: 'header--fixed',
+//   unfixed: 'header--unfixed',
+//   black: "#1e1e1e",
+//   white: "#fff",
+//   animationTriggerElem: '.intro__title'
+// });
+
+const autoText = new AutoText( {
+  text: "фронтенд-разработчик",
+  elem: ".test"
 });
